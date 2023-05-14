9d661a130e70c265bef5d5f86fb171f276bf2320
noor <noor_coder@LAPTOP-LGU6DR6K>
2023-05-14 21:51:12 +0530

    Text change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with docker with change
+          Learn React with docker with final changes
         </a>
       </header>
     </div>
